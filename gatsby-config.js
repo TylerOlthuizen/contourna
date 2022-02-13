1a71351d5ca701ebc70a84a80fd1730393e493aa
tylerolthuizen <tylerolthuizen@gmail.com>
2022-02-13 16:19:00 -0700

    removed typescript support

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,14 +68,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-    {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.ts`,
